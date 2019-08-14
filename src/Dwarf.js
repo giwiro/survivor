456a4e9ab3814c59cf5b4d559204bf6649d3610c
giwiro <giwirodavalos@gmail.com>
2019-08-14 10:47:45 -0500

    next tick approach

--- src/Dwarf.js
+++ src/Dwarf.js
@@ -7,3 +7,13 @@ export const dwarfStandAnimationArray = [
     new PIXI.Rectangle(121, 10, 24, 20),
     new PIXI.Rectangle(159, 10, 24, 20),
 ];
+
+export default class Dwarf {
+    constructor() {
+
+    }
+
+    nextTick() {
+
+    }
+}
--- src/index.js
+++ src/index.js
@@ -13,56 +13,59 @@ const app = new PIXI.Application();
 document.body.appendChild(app.view);
 
 // load the texture we need
-app.loader.add('bunny', bunny).add('dwarf', dwarf).load((loader, resources) => {
-    const textureArray = [];
-    for (let i in dwarfStandAnimationArray) {
-        textureArray.push(new PIXI.Texture(resources.dwarf.texture, dwarfStandAnimationArray[i]));
-    }
-    console.log('textureArray', textureArray);
-    const animatedSprite = new PIXI.AnimatedSprite(textureArray);
+app.loader
+    .add('dwarf', dwarf)
+    .load((loader, resources) => {
+        const textureArray = [];
+        for (let i in dwarfStandAnimationArray) {
+            textureArray.push(new PIXI.Texture(resources.dwarf.texture, dwarfStandAnimationArray[i]));
+        }
+        console.log('textureArray', textureArray);
+        const animatedSprite = new PIXI.AnimatedSprite(textureArray);
 
-    animatedSprite.x = app.renderer.width / 2;
-    animatedSprite.y = app.renderer.height / 2;
-    animatedSprite.scale.set(2, 2);
-    // animatedSprite.loop = false;
+        animatedSprite.x = app.renderer.width / 2;
+        animatedSprite.y = app.renderer.height / 2;
+        animatedSprite.scale.set(2, 2);
+        // animatedSprite.loop = false;
 
-    animatedSprite.animationSpeed = 0.08;
-    animatedSprite.play();
+        animatedSprite.animationSpeed = 0.08;
+        animatedSprite.play();
 
-    app.stage.addChild(animatedSprite);
-    // This creates a texture from a 'bunny.png' image
-    /*const dwarfTexture = resources.dwarf.texture;
-    dwarfTexture.frame = dwarfStandAnimationArray[0];
-    const dwarf = new PIXI.Sprite(dwarfTexture);
+        app.stage.addChild(animatedSprite);
+        // This creates a texture from a 'bunny.png' image
+        /*const dwarfTexture = resources.dwarf.texture;
+        dwarfTexture.frame = dwarfStandAnimationArray[0];
+        const dwarf = new PIXI.Sprite(dwarfTexture);
 
-    dwarf.x = app.renderer.width / 2;
-    dwarf.y = app.renderer.height / 2;
-    dwarf.scale.set(2, 2);
+        dwarf.x = app.renderer.width / 2;
+        dwarf.y = app.renderer.height / 2;
+        dwarf.scale.set(2, 2);
 
-    app.stage.addChild(dwarf);*/
+        app.stage.addChild(dwarf);*/
 
-    // let index = 0;
+        // let index = 0;
 
-    app.ticker.add(() => {
-        // index = (index + 1) % dwarfStandAnimationArray.length;
-        // each frame we spin the bunny around a bit
-        // dwarfTexture.frame = dwarfStandAnimationArray[index];
+        app.ticker.add((delta) => {
+            // console.log('delta', delta);
+            // index = (index + 1) % dwarfStandAnimationArray.length;
+            // each frame we spin the bunny around a bit
+            // dwarfTexture.frame = dwarfStandAnimationArray[index];
+        });
+
+        /*// Setup the position of the bunny
+        bunny.x = app.renderer.width / 2;
+        bunny.y = app.renderer.height / 2;
+
+        // Rotate around the center
+        bunny.anchor.x = 0.5;
+        bunny.anchor.y = 0.5;
+
+        // Add the bunny to the scene we are building
+        app.stage.addChild(bunny);
+
+        // Listen for frame updates
+        app.ticker.add(() => {
+             // each frame we spin the bunny around a bit
+            bunny.rotation += 0.01;
+        });*/
     });
-
-    /*// Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
-
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
-
-    // Add the bunny to the scene we are building
-    app.stage.addChild(bunny);
-
-    // Listen for frame updates
-    app.ticker.add(() => {
-         // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
-    });*/
-});
